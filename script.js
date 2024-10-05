ca515e71b6c431543719b6dd687fed020e879a12
yudisrywn <yudisuryawan7@students.unnes.ac.id>
2024-10-05 09:05:01 +0700

    Menampilkan Product Item dengan JS

--- script.js
+++ script.js
@@ -28,3 +28,44 @@ document.addEventListener("click", function (e) {
     shoppingCart.classList.remove("active");
   }
 });
+
+// product
+let products = {
+  items: [
+    {
+      id: 1,
+      name: "The Alchemist",
+      img: "/images/theAlchemist.jpg",
+      price: 216000,
+      discount: 80 / 100,
+    },
+    {
+      id: 2,
+      name: "Rich Dad Poor Dad",
+      img: "/images/Richdad.jpg",
+      price: 60000,
+      discount: 90 / 100, // discount = 100 -discount = 100 - 90 = 10%
+    },
+    {
+      id: 3,
+      name: "I Want Die But I Want To Eat Tteokbokki",
+      img: "/images/iWantDie.jpg",
+      price: 150000,
+      discount: 70 / 100,
+    },
+    {
+      id: 4,
+      name: "Chicken Soup For The Soul",
+      img: "/images/chickenSoup.jpg",
+      price: 119000,
+      discount: 65 / 100,
+    },
+    {
+      id: 5,
+      name: "Start With Why",
+      img: "/images/startWithWhy.jpg",
+      price: 80000,
+      discount: 75 / 100,
+    },
+  ],
+};
